bed6b42b4692ee89e1f54ce484d6734c355f8cef
Fei Wang <wang.flynn@gmail.com>
2025-01-05 12:32:29 +0800

    count found relic node num

--- ebb/env/luxenv.py
+++ ebb/env/luxenv.py
@@ -801,8 +801,8 @@ class LuxS3Env(gym.Env):
 
       info['_match_total_hidden_relic_nodes_num'] = (
           env_state.relic_nodes_map_weights > 0).sum()
-      info['_match_total_found_relic_nodes_num'] = mm.get_visited_relic_nb_num(
-      )
+      info['_match_total_found_relic_nodes_num'] = (mm.team_point_mass
+                                                    >= TEAM_POINT_MASS).sum()
 
       # Team points stats
       tp0 = raw_obs1['team_points'][mm.player_id]
